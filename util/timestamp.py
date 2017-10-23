aa7ffde28c86c52572b7ee1469e36de7f9df4ca8
horensic <horensic@gmail.com>
2017-10-23 12:23:36 +0900

    filetime 함수 추가

---
+++ util/timestamp.py
@@ -0,0 +1,12 @@
+from datetime import datetime, timedelta
+
+
+def filetime(v, bias=None):
+    """
+    :param v: little-endian binary data in long(integer) type (ex. 131210007740281591)
+    :param bias: timezone option
+    :return: YYYY-MM-DD hh:mm:ss.micros
+    """
+    dt = "{0:x}".format(v)
+    us = int(dt, 16) / 10.
+    return datetime(1601, 1, 1) + timedelta(microseconds=us)
